a519cfb90c199149f4190973cff2af1448e24804
= <642212607@qq.com>
2019-03-02 09:42:46 +0800

    调整项目结构

---
+++ NetworkGraph/view.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+ 
+#from django.http import HttpResponse
+from django.shortcuts import render
+
---
+++ index/index_static/js/fullScreen.js
@@ -0,0 +1,22 @@
+//进入全屏
+function enterFullScreen() {
+    var de = document.documentElement;
+    if (de.requestFullscreen) {
+        de.requestFullscreen();
+    } else if (de.mozRequestFullScreen) {
+        de.mozRequestFullScreen();
+    } else if (de.webkitRequestFullScreen) {
+        de.webkitRequestFullScreen();
+    }
+}
+//退出全屏
+function exitFullScreen() {
+    var de = document;
+    if (de.exitFullscreen) {
+        de.exitFullscreen();
+    } else if (de.mozCancelFullScreen) {
+        de.mozCancelFullScreen();
+    } else if (de.webkitCancelFullScreen) {
+        de.webkitCancelFullScreen();
+    }
+}
